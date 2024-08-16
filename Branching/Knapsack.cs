aaa1862730bebee0444ffbc61aff2f9815b8f07d
577716@student.belgiumcampus.ac.za <577716@student.belgiumcampus.ac.za>
2024-08-16 11:38:53 +0200

    (wip): dual and knapsack

--- Branching/Knapsack.cs
+++ Branching/Knapsack.cs
@@ -1,12 +1,154 @@
-﻿using System;
+﻿/**using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LPR381Project.Common;
+
 
 namespace LPR381Project.Branching.Knapsack
 {
     internal class Knapsack
     {
+        private Tableau table;
+        public int RHS = 0; // remeber to do something with this
+        private int iteration = 0;
+        public int lengt = new Common.Tableau.LenRows(); 
+
+        public array Knapsack(Tableau table)
+        {
+            this.table = table;
+            //find size of inner list
+            //public int lengt = new Common.Tableau.LenRows(); 
+            //check only 1 constraint
+            //gwt tablue into arr so we can manipulate easier
+            Array current = new Array[5;lengt];
+            for (int i = 0; i<lengt; i++)
+            {
+                current[0,i]= i+1;
+                current[1,i]=this.table[0][i] ;
+                current[2,i]=this.table[1][i] ;
+                current[3,i]= 0;
+                current[4,i]= 0;
+                current[5,i]= 0;
+                
+                //list 1; integerindex representing int
+            
+            //list 2: z function coeficcients
+            //list 3; constraint coeficcients
+            //list 4 flag includ4ed or not
+            //list 5 : PINNED OR NOT
+            //list 6 ratio
+            //var RHS*******
+            return current;
+            }
+
+
+        
+        public int Iteration(Array current) 
+        {
+            for (int i = 0; i<lengt; i++){ 
+                //ratio test, rank
+                current[5;i] = current[1,i]/current[2,i];
+
+                //sort according to ratio
+
+                //RHS
+
+                //RHS-L3 if>0 l4=1 ; if ans<0 branch  L5=1 , move positions and branch. 
+            // candidate value = sum flag(l4)*Z(l2)
+
+
+
+            }
+
+
+
+
+
+        }
+        
+
+        
+
+            
+           
+        
+        
+       
+        //some data structure to keep further itterations
+        //itterate: 
+            //RHS-L3 if>0 l4=1 ; if ans<0 branch  L5=1 , move positions and branch. 
+            // candidate value = sum flag(l4)*Z(l2)
+        }
+        
+        
+
+    }
+}
+**/
+using System;
+using System.Collections.Generic;
+using LPR381Project.Common;
+
+namespace LPR381Project.Branching.Knapsack
+{
+    internal class Knapsack
+    {
+        private Tableau table;
+        private int RHS = 0; // This should be set or used later in the code
+        private int iteration = 0;
+        private int length; // To store the number of rows in the tableau
+
+        public Knapsack(Tableau table)
+        {
+            this.table = table;
+            this.length = table.LenRows(); // Initialize length with the number of rows in the tableau
+        }
+
+        public double[,] InitializeKnapsack()
+        {
+            double[,] current = new double[6, length]; // 6 rows for the structure you've described
+
+            for (int i = 0; i < length; i++)
+            {
+                current[0, i] = i + 1;               // Index
+                current[1, i] = table[0][i];         // Z function coefficients
+                current[2, i] = table[1][i];         // Constraint coefficients
+                current[3, i] = 0;                   // Flag for included or not
+                current[4, i] = 0;                   // Pinned or not
+                current[5, i] = 0;                   // Ratio (Z/Constraint)
+            }
+
+            return current;
+        }
+
+        public void Iteration(double[,] current)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                // Ratio test
+                current[5, i] = current[2, i] != 0 ? current[1, i] / current[2, i] : 0;
+
+                // Sorting according to the ratio could be done here (not implemented in this example)
+
+                // RHS - L3 if > 0, L4 = 1; if < 0, branch, L5 = 1, move positions and branch.
+                double result = RHS - current[2, i];
+                if (result > 0)
+                {
+                    current[3, i] = 1; // Flag it as included
+                }
+                else
+                {
+                    current[4, i] = 1; // Pin it (branching would involve more logic)
+                }
+
+                // Calculate the candidate value as the sum of the flagged Z coefficients
+                // This could be done outside of the loop or with more logic
+            }
+
+            // Additional logic for branching and further iterations would be added here
+        }
     }
 }
--- Simplex/DualSimplex.cs
+++ Simplex/DualSimplex.cs
@@ -3,10 +3,160 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using LPR381Project.Common;
+using System.Diagnostics;
 
 namespace LPR381Project.Simplex.Dual
 {
-    internal class DualSimplex
+     enum DualPivotKind
     {
+        Optimal,
+        Infeasible,
+        SubOptimal,
+    }
+
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+    internal class DualSimplex
+    {private Tableau table;
+        private int iteration = 0;
+
+        public DualSimplex(Tableau table)
+        {
+            this.table = table;
+        }
+
+        // Main solving method
+        public Tableau Solve()
+        {
+            DualPivotKind status = DualPivotKind.SubOptimal;
+
+            this.table.WriteTable(iteration);
+            iteration++;
+
+            while (status != DualPivotKind.Optimal)
+            {
+                status = this.Pivot();
+
+                if (status != DualPivotKind.Optimal)
+                {
+                    this.table.WriteTable(iteration);
+                    iteration++;
+                }
+            }
+
+            Console.WriteLine();
+            return this.table;
+        }
+  // Perform a pivot operation
+        public DualPivotKind Pivot()
+        {
+            int pivotRow, pivotCol;
+
+           
+            pivotCol = this.table.GetPivotCol();  // Implement this method to select the pivot column
+            pivotRow = this.table.GetPivotRow(pivotCol);  // Implement this method to select the pivot row
+            if (pivotRow == -1 || pivotCol == -1)
+            {
+                return DualPivotKind.Optimal;
+            }
+
+            if (pivotRow == -1)
+            {
+                // Handle infeasibility case if needed
+                Console.WriteLine($"Problem is infeasible at iteration {iteration}");
+                Environment.Exit(1);
+            }
+
+            this.ManualPivot(pivotRow, pivotCol);
+
+            return DualPivotKind.SubOptimal;
+        }
+
+        // Perform the pivot operation manually
+        public void ManualPivot(int pivotRow, int pivotCol)
+        {
+            double[][] tmpTable = this
+                           .table.table.Select(innerList => innerList.ToArray())
+                           .ToArray();
+
+            // Normalize pivot row
+            for (int i = 0; i < tmpTable[0].Length; i++)
+            {
+                var a = tmpTable[pivotRow][i];
+                var b = tmpTable[pivotRow][pivotCol];
+
+                this.table.table[pivotRow][i] = a / b;
+            }
+
+            // Update other rows
+            for (int row = 0; row < tmpTable.Length; row++)
+            {
+                if (row == pivotRow)
+                {
+                    continue;
+                }
+
+                int rowSize = tmpTable[row].Length;
+                for (int col = 0; col < rowSize; col++)
+                {
+                    var a = tmpTable[row][col];
+                    var b = tmpTable[row][pivotCol];
+                    var c = this.table.table[pivotRow][col];
+
+                    this.table.table[row][col] = a - (b * c);
+                }
+            }
+        }
+
+        // Method to get the pivot column
+        public int GetPivotCol()
+        {
+            int numCols = 1; // Assuming table is a List<List<double>>
+            //use get row lengt
+            int pivotCol = -1;
+            double mostNegativeCost = 0;
+
+            for (int col = 0; col < numCols - 1; col++) // Last column is RHS
+            {
+                if (this.table.table[0][col] < mostNegativeCost)
+                {
+                    mostNegativeCost = this.table.table[0][col];
+                    pivotCol = col;
+                }
+            }
+
+            return pivotCol;
+        }
+
+        // Method to get the pivot row
+        public int GetPivotRow(int pivotCol)
+        {
+            int numRows = this.table.LenRows();
+            int pivotRow = -1;
+            double minRatio = double.MaxValue;
+
+            for (int row = 1; row < numRows; row++) // Skip the objective row
+            {
+                if (this.table.table[row][pivotCol] > 0)
+                {
+                    double ratio = this.table.table[row][^1] / this.table.table[row][pivotCol]; // RHS / pivot column value
+
+                    if (ratio < minRatio)
+                    {
+                        minRatio = ratio;
+                        pivotRow = row;
+                    }
+                }
+            }
+
+            return pivotRow;
+        
+    }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
     }
 }
